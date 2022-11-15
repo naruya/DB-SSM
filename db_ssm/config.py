4d97eefba47c2f23e4c7b692b46bfed8486c59e6
naruya <naruya.kondo@gmail.com>
2022-11-15 12:49:32 +0000

    update

--- db_ssm/config.py
+++ db_ssm/config.py
@@ -15,7 +15,7 @@ def get_args(args=None):
     parser.add_argument("--model", type=str, default="rssm")
     parser.add_argument("--s_dim", type=int, default=64)
     parser.add_argument("--z_dim", type=int, default=64)
-    parser.add_argument("--v_dim", type=int, default=6)
+    parser.add_argument("--v_dim", type=int, default=3)
     parser.add_argument("--a_dim", type=int, default=0)
     parser.add_argument("--h_dim", type=int, default=512)
     parser.add_argument("--size", type=int, default=128)
@@ -34,8 +34,8 @@ def get_args(args=None):
     parser.add_argument("--epochs", type=int, default=1000)
     parser.add_argument('--max_norm', type=float, default=1e+7)
 
-    parser.add_argument("--freq_valid", type=int, default=10)
-    parser.add_argument("--freq_save", type=int, default=10)
+    parser.add_argument("--freq_valid", type=int, default=100)
+    parser.add_argument("--freq_save", type=int, default=100)
 
     return parser.parse_args(args)
 
--- db_ssm/datasets.py
+++ db_ssm/datasets.py
@@ -9,43 +9,32 @@ from torch.utils.data import Dataset, DataLoader
 class MyDataset(Dataset):
     def __init__(self, data_dir, mode, T):
         self.T = T
-        self.H = 1
 
-        vid_paths = sorted(glob.glob(os.path.join(data_dir, mode, "video", "*.gif")))
-        viw_paths = sorted(glob.glob(os.path.join(data_dir, mode, "view", "*.npy")))
-        # mot_paths = sorted(glob.glob(os.path.join(data_dir, mode, "motion", "*.npy")))
+        vid_paths = sorted(glob.glob(os.path.join(data_dir, "video", "*.gif")))
+        viw_paths = sorted(glob.glob(os.path.join(data_dir, "view", "*.npy")))
 
-        # use_head
-        # self.vid = np.array([vread(path) for path in vid_paths]).astype(np.uint8)
-        self.vid = np.array([vread(path)[:self.T+self.H] for path in vid_paths]).astype(np.uint8)
+        if mode == 'train':
+            indices = list(np.arange(len(vid_paths)))
+            indices = list(set(indices) - set(list(np.arange(0, len(indices), 7))))
+        else:
+            indices = list(np.arange(0, len(vid_paths), 7))
+
+        vid_paths = np.array(vid_paths)[indices]
+        viw_paths = np.array(viw_paths)[indices]
+
+        self.vid = np.array([vread(path) for path in vid_paths]).astype(np.uint8)
         self.viw = np.array([np.load(path) for path in viw_paths]).astype(np.float32)
-        # self.mot = np.array([np.load(path) for path in mot_paths])]).astype(np.float32)
-
-        # bug of moviepy?
-        # if np.sum(self.vid[-2] - self.vid[-1]) == 0:
-        #     self.vid = self.vid[:-1]
-
-        self.viw = np.deg2rad(self.viw[..., 3:])  # xyzrpy
-        self.viw = np.concatenate([np.sin(self.viw), np.cos(self.viw)], axis=2)
-
-        assert len(self.vid) == len(self.viw), \
-            "incorrect data length detected!!!"
-        self.N, self.L = self.vid.shape[:2]
-        self.N_per_vid = self.L - self.T + 1 - self.H
 
         print(mode, self.vid.shape, self.viw.shape)
 
     def __len__(self):
-        return self.N * self.N_per_vid
+        return len(self.vid)
 
-    def __getitem__(self, idx_t):
-        idx = idx_t // self.N_per_vid
-        t = idx_t % self.N_per_vid
-        x = self.vid[idx,t:t+self.T+1]
+    def __getitem__(self, idx):
+        x = self.vid[idx]
+        v = self.viw[idx]
         x = np.transpose(x, [0,3,1,2])
-        x_0, x = x[0], x[1:]
-        v = self.viw[idx,t+1:t+self.T+1]
-        # a = self.mot[t+1:t+self.T+1]
+        x_0, x, v = x[0], x[1:], v[1:]
         return x_0, x, v
 
 
@@ -53,11 +42,8 @@ class MyDataset(Dataset):
 class MyValidDataset(MyDataset):
     def __init__(self, dataset, T_val):
         self.T = T_val
-        self.N = dataset.N
-        self.N_per_vid = dataset.N_per_vid
         self.vid = dataset.vid
         self.viw = dataset.viw
-        # self.mot = dataset.mot
 
 
 class MyLoader(DataLoader):
